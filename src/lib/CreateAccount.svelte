7f0e150a2b0c7ed0684b84208c95f91941d6f90f
Kiner Shah <kiner.shah1@gmail.com>
2023-06-06 12:30:44 +0530

    Changed Create Account page and Dashboard page

--- src/lib/CreateAccount.svelte
+++ src/lib/CreateAccount.svelte
@@ -1,19 +1,71 @@
+<script>
+    import { PUBLIC_API_URL } from "$env/static/public";
+    import { goto } from "$app/navigation";
+
+    let emailid;
+    let username;
+    let password;
+    let confirmpassword;
+    let createAccountError;
+
+    function validateFormAndCreateAccount() {
+        if (typeof emailid !== "string" || typeof username !== "string" || typeof password !== "string" || typeof confirmpassword !== "string") {
+            return false;
+        }
+        if (password !== confirmpassword) {
+            createAccountError = "Passwords don't match";
+            setTimeout(() => createAccountError = undefined, 10000);
+            return;
+        }
+        let url = new URL("/createaccount", PUBLIC_API_URL);
+        const sendRequest = () => {
+            fetch(url.toString(), {
+                method: "POST",
+                body: JSON.stringify({emailid, username, password})
+            }).then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error(response.statusText, {
+                    cause: response.status
+                });
+            }).then(jsonObj => {
+                // localStorage.setItem("loggedInUser", JSON.stringify(jsonObj));
+                // loggedInUser.set(jsonObj);
+                // goto("/dashboard");
+            }).catch(error => {
+                if ("cause" in error) {
+                    createAccountError = error.message;
+                }
+                else {
+                    createAccountError = "Server is down";
+                }
+                // The message will disappear after 10 seconds.
+                setTimeout(() => createAccountError = undefined, 10000);
+            });
+        };
+        sendRequest();
+    }
+</script>
 <h1>Create Account</h1>
 <section>
-    <form action="?/validateAndCreateAccount" method="post">
+    <form method="post" on:submit|preventDefault={validateFormAndCreateAccount}>
+        {#if createAccountError !== undefined}
+        <p id="error-message" bind:innerHTML={createAccountError} contenteditable="false"></p>
+        {/if}
         <label for="emailid">Email id</label>
-        <input name="emailid" id="emailid" type="email" required />
+        <input name="emailid" id="emailid" type="email" bind:value={emailid} required />
 
         <label for="username">Username</label>
-        <input name="username" id="username" type="text" required />
+        <input name="username" id="username" type="text" bind:value={username} required />
 
         <label for="password">Password</label>
-        <input name="password" id="password" type="password" required />
+        <input name="password" id="password" type="password" bind:value={password} required />
 
         <label for="confirmpassword">Confirm Password</label>
-        <input name="confirmpassword" id="confirmpassword" type="password" required />
+        <input name="confirmpassword" id="confirmpassword" type="password" bind:value={confirmpassword} required />
 
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Submit" />
     </form>
 </section>
 <style>
@@ -46,5 +98,13 @@
     }
     section form input[type=submit]:hover {
         background-color: rgb(238, 218, 103);
+    }
+    #error-message {
+        background-color: rgb(133, 48, 48);
+        color: white;
+        width: 100%;
+        border: none;
+        text-align: center;
+        padding: 1%;
     }
 </style>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,8 @@
 <script>
+    let moment_count = 0;
 
+    let moments_per_page = [10, 20, 50, 100];
+    let selected_moments_per_page = 0;
 </script>
 
 <svelte:head>
@@ -7,29 +10,85 @@
 </svelte:head>
 
 <section id="moments-header">
-    <input type="search" id="searchbar" placeholder="Search title">
-    <section>
-        <label for="sort-by">Sort by</label>
-        <select id="sort-by" name="sort-by">
-            <option value="date-asc">Date posted (ascending)</option>
-            <option value="date-desc">Date posted (descending)</option>
-        </select>
+    <section id="moments-summary">
+        <h3>Total moments: {moment_count}</h3>
+        <button id="add-new-moment">Add New Moment</button>
+    </section>
+    <section id="moments-search-bar-and-sort">
+        <input type="search" id="searchbar" placeholder="Search title">
+        <section id="moments-sort-by">
+            <label for="sort-by">Sort by</label>
+            <select id="sort-by" name="sort-by">
+                <option value="date-asc">Date posted (ascending)</option>
+                <option value="date-desc">Date posted (descending)</option>
+            </select>
+        </section>
+    </section>
+</section>
+
+<section id="moments-footer">
+    <section id="moments-page">
+        <button>Prev</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>Next</button>
+    </section>
+
+    <section id="moments-per-page">
+        <span>Moments per page: &nbsp;</span>
+        {#each moments_per_page as moment_per_page, index}
+        <button on:click={() => {selected_moments_per_page = index;}} class:moments-per-page-active={index === selected_moments_per_page}>{moment_per_page}</button>
+        {/each}
     </section>
 </section>
 
 <style>
-    #moments-header {
+    #moments-summary,
+    #moments-search-bar-and-sort {
         display: flex;
         width: 80%;
         flex-direction: row;
         justify-content: space-between;
-        margin-left: auto;
-        margin-right: auto;
+        margin: 1% auto 1% auto;
         column-gap: 20px;
         align-items: center;
     }
     #searchbar {
         height: 50px;
         width: 100%;
+    }
+    button {
+        border-style: none;
+        background-color: rgb(250, 229, 107);
+        color: rgba(144,128,39,1);
+        padding: 2%;
+    }
+    button:hover {
+        background-color: rgb(238, 218, 103);
+    }
+    .moments-per-page-active {
+        background-color: rgb(238, 218, 103);
+    }
+    #moments-footer {
+        width: 80%;
+        display: flex;
+        flex-direction: row;
+        margin-bottom: 1%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    #moments-page {
+        display: flex;
+        width: 50%
+    }
+    #moments-per-page {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        width: 50%
     }
 </style>
