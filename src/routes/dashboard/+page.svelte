4bb92112a142fc9e22752224e2160a45626fdfe9
Kiner Shah <kiner.shah1@gmail.com>
2023-06-12 17:11:23 +0530

    Added pagination logic, moments list

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,8 +1,59 @@
 <script>
-    let moment_count = 0;
+    // 20 December 2012 at 08:30:00 pm
+    // const event = new Date(Date.UTC(2012, 11, 20, 15, 0, 0));
+    // console.log(event.toLocaleString('en-GB', { dateStyle: 'long', timeStyle: 'medium', timeZone: 'IST', hour12: 'true' }));
+    let moment_count = 2;
 
     let moments_per_page = [10, 20, 50, 100];
-    let selected_moments_per_page = 0;
+    let selected_index_for_moments_per_page = 0;
+
+    let page_range_start = 0;
+    const page_range_length = 10;
+    let current_page = 1;
+    let max_pages = 100;
+
+    let moments_data = [
+        {
+            id: "moment0",
+            title: "Title 0",
+            description:
+                "This is a very long summary for this moment. I have no idea what to do to get this summary, so I had no choice but to use this dummy",
+            created_by_username: "user1",
+            created_date_time: "9 Jun 2023 3:16:33 pm",
+            last_edit_date_time: "9 Jun 2023 3:16:33 pm",
+            image_url: "https://picsum.photos/200",
+            image_caption: "This is a sample caption for image",
+        },
+        {
+            id: "moment1",
+            title: "Title 1",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed lorem vehicula, pellentesque dolor quis, convallis felis. Praesent sodales neque eget erat tincidunt, id iaculis est accumsan. Duis non erat congue, fermentum magna vel, interdum mauris. Phasellus at augue magna. Curabitur tempus, ligula maximus fermentum dapibus, velit mi dignissim elit, sit amet cursus nulla nisl vel magna. Sed ut nisi sit amet urna maximus laoreet vitae vitae quam. Ut malesuada est eu dapibus venenatis. Nulla lorem enim, viverra condimentum erat suscipit, blandit mollis eros. Fusce quis vulputate ligula. Praesent neque urna, vestibulum sed libero at, varius tincidunt turpis.",
+            created_by_username: "user1",
+            created_date_time: "9 Jun 2023 3:20:33 pm",
+            last_edit_date_time: "9 Jun 2023 3:20:33 pm",
+            image_url: "https://picsum.photos/200/300",
+            image_caption: "This is a sample caption for image",
+        },
+    ];
+
+    function changeToPage(page_number) {
+        if (page_number <= 0 || page_number > max_pages) {
+            return;
+        }
+        current_page = page_number;
+        if (current_page < page_range_start + 1) {
+            page_range_start = current_page - 1;
+        } else if (current_page > page_range_start + page_range_length) {
+            page_range_start = current_page - page_range_length;
+        }
+        console.log(
+            "Current page is now: " +
+                current_page +
+                " page range start: " +
+                page_range_start
+        );
+    }
 </script>
 
 <svelte:head>
@@ -15,7 +66,8 @@
         <button id="add-new-moment">Add New Moment</button>
     </section>
     <section id="moments-search-bar-and-sort">
-        <input type="search" id="searchbar" placeholder="Search title">
+        <input type="search" id="searchbar" placeholder="Search title" />
+        <button>Search</button>
         <section id="moments-sort-by">
             <label for="sort-by">Sort by</label>
             <select id="sort-by" name="sort-by">
@@ -26,22 +78,64 @@
     </section>
 </section>
 
+<section id="moments-list">
+    {#each moments_data as moment}
+        <section class="moments-list-item">
+            <section class="moments-list-item-image-section">
+                <img
+                    class="moments-list-item-image"
+                    src={moment.image_url}
+                    alt={moment.image_caption}
+                />
+            </section>
+            <section class="moments-list-item-summary">
+                <h3>{moment.title}</h3>
+                <!-- This substring can be useful when description is too large
+                    may improve memory consumption if a view of this string is used -->
+                <span>{moment.description.substring(0, 200)}</span>
+                <section class="moments-list-item-more-info">
+                    <section class="moments-list-item-meta">
+                        <span>Created {moment.created_date_time}</span> &#8226
+                        <span>Last modified {moment.last_edit_date_time}</span>
+                    </section>
+                    <button on:click={() => {}}>Read more</button>
+                </section>
+            </section>
+        </section>
+    {/each}
+</section>
+
 <section id="moments-footer">
     <section id="moments-page">
-        <button>Prev</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>Next</button>
+        <button
+            disabled={current_page == 1}
+            on:click={() => changeToPage(current_page - 1)}>Prev</button
+        >
+        <!-- display 10 page buttons at a time -->
+        {#each { length: page_range_length } as _, i}
+            <button
+                on:click={() => changeToPage(page_range_start + i + 1)}
+                class:active-page={current_page == page_range_start + i + 1}
+                >{page_range_start + i + 1}</button
+            >
+        {/each}
+        <button
+            disabled={current_page == max_pages}
+            on:click={() => changeToPage(current_page + 1)}>Next</button
+        >
     </section>
 
     <section id="moments-per-page">
-        <span>Moments per page: &nbsp;</span>
+        <span>Moments per page:&nbsp;</span>
         {#each moments_per_page as moment_per_page, index}
-        <button on:click={() => {selected_moments_per_page = index;}} class:moments-per-page-active={index === selected_moments_per_page}>{moment_per_page}</button>
+            <button
+                on:click={() => {
+                    selected_index_for_moments_per_page = index;
+                }}
+                class:moments-per-page-active={index ===
+                    selected_index_for_moments_per_page}
+                >{moment_per_page}</button
+            >
         {/each}
     </section>
 </section>
@@ -64,13 +158,17 @@
     button {
         border-style: none;
         background-color: rgb(250, 229, 107);
-        color: rgba(144,128,39,1);
+        color: rgba(144, 128, 39, 1);
         padding: 2%;
     }
-    button:hover {
+    button:hover,
+    button:disabled {
         background-color: rgb(238, 218, 103);
     }
     .moments-per-page-active {
+        background-color: rgb(238, 218, 103);
+    }
+    .active-page {
         background-color: rgb(238, 218, 103);
     }
     #moments-footer {
@@ -83,12 +181,51 @@
     }
     #moments-page {
         display: flex;
-        width: 50%
+        width: 50%;
     }
     #moments-per-page {
         display: flex;
         justify-content: flex-end;
         align-items: center;
-        width: 50%
+        width: 50%;
+    }
+
+    #moments-list {
+        width: 80%;
+        margin: 0px auto 2% auto;
+    }
+    .moments-list-item {
+        display: flex;
+        flex-direction: row;
+        column-gap: 10px;
+        align-items: center;
+        width: 100%;
+        border: 1px solid black;
+        border-radius: 10px;
+        padding: 1%;
+        margin-top: 1%;
+        background-color: rgba(250, 228, 107, 0.26);
+    }
+    .moments-list-item-image-section {
+        width: 80px;
+        height: 100px;
+    }
+    .moments-list-item-image {
+        width: 100%;
+        height: 100%;
+    }
+    .moments-list-item-summary {
+        width: 100%;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+    .moments-list-item-more-info {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        font-size: 0.75em;
+        margin-top: 1%;
+        align-items: center;
     }
 </style>
