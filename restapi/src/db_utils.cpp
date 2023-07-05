2a957343b66c7d8ddef30ad86a2befe45283620c
Kiner Shah <kiner.shah1@gmail.com>
2023-07-05 16:07:39 +0530

    Modified about us page

--- restapi/src/db_utils.cpp
+++ restapi/src/db_utils.cpp
@@ -9,7 +9,7 @@ namespace mkm
     std::variant<User, ErrorCode> get_user_details(const std::string &username)
     {
         // TODO: replace hardcoded string with values from some environment/properties file
-        pqxx::connection c("dbname=mkm_db user=mkm_user password=U5er@Mkm");
+        pqxx::connection c("dbname=mkm_db user=mkm_user password=<DBPASSWORD>");
 
         pqxx::read_transaction transaction(c);
 
@@ -36,7 +36,7 @@ namespace mkm
     bool is_password_valid(const std::string &input_password, const std::string &stored_password_hash)
     {
         // TODO: replace hardcoded string with values from some environment/properties file
-        pqxx::connection c("dbname=mkm_db user=mkm_user password=U5er@Mkm");
+        pqxx::connection c("dbname=mkm_db user=mkm_user password=<DBPASSWORD>");
 
         pqxx::read_transaction transaction(c);
 
@@ -75,7 +75,7 @@ namespace mkm
     bool create_new_account(const User& user_details, const std::string& password)
     {
         // TODO: replace hardcoded string with values from some environment/properties file
-        pqxx::connection c("dbname=mkm_db user=mkm_user password=U5er@Mkm");
+        pqxx::connection c("dbname=mkm_db user=mkm_user password=<DBPASSWORD>");
 
         pqxx::work transaction(c);
 
@@ -110,7 +110,7 @@ namespace mkm
     bool add_new_moment(const Moment& moment)
     {
         // TODO: replace hardcoded string with values from some environment/properties file
-        pqxx::connection c("dbname=mkm_db user=mkm_user password=U5er@Mkm");
+        pqxx::connection c("dbname=mkm_db user=mkm_user password=<DBPASSWORD>");
 
         pqxx::work transaction(c);
 
@@ -180,7 +180,7 @@ namespace mkm
     bool update_moment(const Moment& moment)
     {
         // TODO: replace hardcoded string with values from some environment/properties file
-        pqxx::connection c("dbname=mkm_db user=mkm_user password=U5er@Mkm");
+        pqxx::connection c("dbname=mkm_db user=mkm_user password=<DBPASSWORD>");
 
         pqxx::work transaction(c);
 
@@ -247,7 +247,7 @@ namespace mkm
     bool delete_moment(const std::string& username, uint64_t moment_id)
     {
         // TODO: replace hardcoded string with values from some environment/properties file
-        pqxx::connection c("dbname=mkm_db user=mkm_user password=U5er@Mkm");
+        pqxx::connection c("dbname=mkm_db user=mkm_user password=<DBPASSWORD>");
 
         pqxx::work transaction(c);
 
@@ -275,7 +275,7 @@ namespace mkm
     uint64_t get_moment_count(const std::string& username)
     {
         // TODO: replace hardcoded string with values from some environment/properties file
-        pqxx::connection c("dbname=mkm_db user=mkm_user password=U5er@Mkm");
+        pqxx::connection c("dbname=mkm_db user=mkm_user password=<DBPASSWORD>");
 
         pqxx::read_transaction transaction(c);
 
@@ -295,7 +295,7 @@ namespace mkm
     std::variant< std::vector<Moment>, ErrorCode > get_moments_list(const std::string& username, uint32_t page_size, uint64_t current_page, std::optional<std::string> sort_by, std::optional<std::string> search)
     {
         // TODO: replace hardcoded string with values from some environment/properties file
-        pqxx::connection c("dbname=mkm_db user=mkm_user password=U5er@Mkm");
+        pqxx::connection c("dbname=mkm_db user=mkm_user password=<DBPASSWORD>");
 
         pqxx::read_transaction transaction(c);
         try
@@ -358,7 +358,7 @@ namespace mkm
     std::variant<Moment, ErrorCode> get_moment_details(const std::string& username, uint64_t id)
     {
         // TODO: replace hardcoded string with values from some environment/properties file
-        pqxx::connection c("dbname=mkm_db user=mkm_user password=U5er@Mkm");
+        pqxx::connection c("dbname=mkm_db user=mkm_user password=<DBPASSWORD>");
 
         pqxx::read_transaction transaction(c);
 
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,6 +4,7 @@
 
 <h1>About</h1>
 
+<p>This web app is created by Kiner Shah who is a Software Engineer.</p>
 <style>
     h1 {
         text-align: center;
